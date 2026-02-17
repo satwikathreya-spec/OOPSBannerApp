ba22b4f5f466b112c827dd01139bf813db44e792
satwikathreya-spec <satwikathreyakanala@gmai.com>
2026-02-17 13:58:39 +0530

    printing oops usin * and String.join()

--- OOPSBanner2.java
+++ OOPSBanner2.java
@@ -1,36 +1,64 @@
-public class OOPSBanner2 {
-
+public class OOPSBanner2{
     public static void main(String[] args) {
 
         // Line 1
-        String line1 = "  *****  " + "  *****  " + "  ****** " + "  ****** ";
+        String line1 = String.join("",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** ");
         System.out.println(line1);
 
         // Line 2
-        String line2 = " *     * " + " *     * " + " *      *" + " *      *";
+        String line2 = String.join("",
+                " *     * ",
+                " *     * ",
+                " *      *",
+                " *      *");
         System.out.println(line2);
 
         // Line 3
-        String line3 = "*       *" + "*       *" + "*       *" + "*       ";
+        String line3 = String.join("",
+                "*       *",
+                "*       *",
+                "*       *",
+                "*       ");
         System.out.println(line3);
 
         // Line 4
-        String line4 = "*       *" + "*       *" + "  ****** " + "  *****  ";
+        String line4 = String.join("",
+                "*       *",
+                "*       *",
+                "  ****** ",
+                "  *****  ");
         System.out.println(line4);
 
         // Line 5
-        String line5 = "*       *" + "*       *" + " *       " + "       * ";
+        String line5 = String.join("",
+                "*       *",
+                "*       *",
+                " *       ",
+                "       * ");
         System.out.println(line5);
-
-        // Line 6
-        String line6 = " *     * " + " *     * " + " *       " + "*      * ";
+       // Line 6
+        String line6 = String.join("",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "*      * ");
         System.out.println(line6);
 
+        
         // Line 7
-        String line7 = "  *****  " + "  *****  " + " *       " + " ******  ";
+        String line7 = String.join("",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                " ******  ");
         System.out.println(line7);
     }
 }
+
 
 
 
